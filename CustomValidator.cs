b3fdd344615f072a33cca6e740a539ff2c0e3b13
Victor Rolim <victor.rolim@rockcontent.com>
2022-04-21 16:19:50 -0300

    finishing the challenge

--- CustomValidator.cs
+++ CustomValidator.cs
@@ -1,26 +1,91 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
-public class CustomValidator
+public static class CustomValidator
 {
     public static Func<object, ValidationResult> Brackets(string errorMessage = null)
     {
         return input =>
         {
             if (input == null)
-                return new ValidationResult(errorMessage ?? "Value is required");
+                return new ValidationResult(
+                    errorMessage ?? "Value is required");
 
             string text = input as string;
 
-            char[] charArr = text.ToCharArray();
-
-            foreach (char ch in charArr)
+            if (!ContainsOnlyBrackets(text))
             {
-                Console.WriteLine(ch);
+                return new ValidationResult(
+                    errorMessage ?? "The string must contain only brackets");
             }
 
-            return true
-                    ? new ValidationResult($"Invalid brackets!")
-                    : new ValidationResult($"The brackets are valid!");
+            char[] bracketList = text.ToCharArray();
+            var bracketQueueList = new List<char>();
+            var isValid = true;
+
+            foreach (char bracket in bracketList)
+            {
+                if (IsOpeningBracket(bracket))
+                {
+                    bracketQueueList.Add(bracket);
+
+                    continue;
+                }
+
+                if (!bracketQueueList.Any())
+                {
+                    isValid = false;
+                }
+
+                if (bracket.ClosesBracket(bracketQueueList.Last()))
+                {
+                    bracketQueueList = bracketQueueList.SkipLast(1).ToList();
+
+                    continue;
+                }
+
+                isValid = false;
+            }
+
+            return isValid
+                    ? new ValidationResult($"The brackets {text} are valid!")
+                    : new ValidationResult($"Invalid brackets!");
+        };
+    }
+
+    public static bool ContainsOnlyBrackets(string input)
+    {
+        Regex Validator = new Regex(@"^[([{}\])]+$");
+
+        return Validator.IsMatch(input);
+    }
+
+    public static bool IsOpeningBracket(char bracket)
+    {
+        var openingBrackets = new List<char> { '(', '[', '{' };
+        bool isOpeningBracket = false;
+
+        foreach (char openingBracket in openingBrackets)
+        {
+            if (bracket == openingBracket)
+            {
+                isOpeningBracket = true;
+
+                return isOpeningBracket;
+            }
+        }
+
+        return isOpeningBracket;
+    }
+
+    public static bool ClosesBracket(this char closingBracket, char openingBracket)
+    {
+        return openingBracket switch
+        {
+            '(' => closingBracket == ')',
+            '[' => closingBracket == ']',
+            '{' => closingBracket == '}',
+            _ => false,
         };
     }
 }
