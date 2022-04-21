f6cde7265033943df381f39827902099259fd3c9
Victor Rolim <victor.rolim@rockcontent.com>
2022-04-21 07:43:24 -0300

    first commit

---
+++ CustomValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CustomValidator
+{
+    public static Func<object, ValidationResult> Brackets(string errorMessage = null)
+    {
+        return input =>
+        {
+            if (input == null)
+                return new ValidationResult(errorMessage ?? "Value is required");
+
+            string text = input as string;
+
+            char[] charArr = text.ToCharArray();
+
+            foreach (char ch in charArr)
+            {
+                Console.WriteLine(ch);
+            }
+
+            return true
+                    ? new ValidationResult($"Invalid brackets!")
+                    : new ValidationResult($"The brackets are valid!");
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Sharprompt;
+
+const string bracketsText =
+    "Type your brackets to validate";
+
+var emailAnswer = Prompt.Input<string>(bracketsText, validators: new[] { CustomValidator.Brackets() });
